913985c8c2fbe8e0daa0b7c6290cb80e195aeeff
GREEDSSPIRIT <124819472+GREEDSSPIRIT@users.noreply.github.com>
2023-02-09 12:02:58 -0800

    Added Files
    Initial start of project

---
+++ javascript.js
@@ -0,0 +1,81 @@
+<><script>
+  function myFunction() { }
+  var checkBox = document.getElementById("myCheck");
+  var text = document.getElementById("text");
+  if (checkBox.checked == true){text.style.display = "block"};
+  } else {text.style.display = "none"};
+  }
+  }
+</script><script>
+    function myFunction2() { }
+    var checkBox = document.getElementById("myCheck2");
+    var text = document.getElementById("text2");
+    if (checkBox.checked == true){text.style.display = "block"};
+    } else {text.style.display = "none"};
+    }
+    }
+  </script><script>
+    function myFunction3() { }
+    var checkBox = document.getElementById("myCheck3");
+    var text = document.getElementById("text3");
+    if (checkBox.checked == true){text.style.display = "block"};
+    } else {text.style.display = "none"};
+    }
+    }
+  </script><script>
+    function myFunction4() { }
+    var checkBox = document.getElementById("myCheck4");
+    var text = document.getElementById("text4");
+    if (checkBox.checked == true){text.style.display = "block"};
+    } else {text.style.display = "none"};
+    }
+    }
+  </script><script>
+    function myFunction5() { }
+    var checkBox = document.getElementById("myCheck5");
+    var text = document.getElementById("text5");
+    if (checkBox.checked == true){text.style.display = "block"};
+    } else {text.style.display = "none"};
+    }
+    }
+  </script><script>
+    function myFunction6() { }
+    var checkBox = document.getElementById("myCheck6");
+    var text = document.getElementById("text6");
+    if (checkBox.checked == true){text.style.display = "block"};
+    } else {text.style.display = "none"};
+    }
+    }
+  </script><script>
+    function myFunction7() { }
+    var checkBox = document.getElementById("myCheck7");
+    var text = document.getElementById("text7");
+    if (checkBox.checked == true){text.style.display = "block"};
+    } else {text.style.display = "none"};
+    }
+    }
+  </script><script>
+    function myFunction8() { }
+    var checkBox = document.getElementById("myCheck8");
+    var text = document.getElementById("text8");
+    if (checkBox.checked == true){text.style.display = "block"};
+    } else {text.style.display = "none"};
+    }
+    }
+  </script><script>
+    function myFunction9() { }
+    var checkBox = document.getElementById("myCheck9");
+    var text = document.getElementById("text9");
+    if (checkBox.checked == true){text.style.display = "block"};
+    } else {text.style.display = "none"};
+    }
+    }
+  </script><script>
+    function myFunction10() { }
+    var checkBox = document.getElementById("myCheck10");
+    var text = document.getElementById("text10");
+    if (checkBox.checked == true){text.style.display = "block"};
+    } else {text.style.display = "none"};
+    }
+    }
+  </script></>
